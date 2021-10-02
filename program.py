2e6fbcd1d030c045dd5af5867a7774a23205ad6b
agvs03 <62472424+agvs03@users.noreply.github.com>
2021-10-02 07:12:57 +0530

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,4 @@
 import csv
-
 import time
 import os, psutil
 from humanfriendly import format_timespan
@@ -14,7 +13,6 @@ for i in dicfile:
 frequency = {}
 sourcefile = open( 't8.shakespeare.txt', 'r+' )
 tempfile=open( 't8.shakespeare.translated.txt', 'w' )
-
 #Fetch each i from sourcefile.
 for i in sourcefile:
     #Fetch each word from i.
@@ -32,27 +30,20 @@ for i in sourcefile:
       #If word not in mydict{}, write the word. 
       else:
         tempfile.write(word+" ")
-
 tempfile.close()
-
 #Sort the frequency{} in ascending order.
 occurence = OrderedDict(sorted(frequency.items()))
-
 csvwriter=open("frequency.csv","w")
 freq=csv.writer(csvwriter)
 freq.writerow(['English word','French word','Frequency'])
-
 #write the frequency of each translated word in csv
 for key in list(occurence.keys()):
     freq.writerow([key,mydic[key],occurence[key]])
 csvwriter.close()
 end=time.time()
-
-
 #calculate time taken and memory used
 str1="Time to process: "+format_timespan(end-start)
 str2="Memory used: "+str(psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)+" MB"
-
 with open("performance.txt","w") as per:
     per.write(str1+"\n"+str2)
 
